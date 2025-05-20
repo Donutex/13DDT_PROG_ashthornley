b0ab44191f944f3246602bf31359286ff7daa99f
Ash Thornley <ashnthornley@gmail.com>
2025-05-21 10:41:08 +1200

    more testing to see if this works, I added in some temporary ways to store users data in text files, this will be changed in the future

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ root.title("Like A Knife Through Clutter")
 
 # functions that will let the buttons do something
 def item_page():
+    def add_item():
+        # function to add an item to your list ---- at the moment it will store this in a text file, this will be changed to a database later
+        item_name = item_name_entry.get()
+        item_price = item_price_entry.get()
+        item_condition = item_condition_entry.get()
+        with open("items.txt", "a") as file:
+            file.write(f"{item_name}, {item_price}, {item_condition}\n")
+            
+        item_name_entry.delete(0, END)
+        item_price_entry.delete(0, END)
+        item_condition_entry.delete(0, END)
+        
     root.withdraw()
     item_window = Toplevel(root)
     item_window.title("Item Page")
@@ -19,6 +31,26 @@ def item_page():
     # Title for the page placed into the header frame
     title = Label(header_frame, text="Like A Knife Through Clutter - your items", font=("papyrus", 20, "bold"))
     title.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
+
+    # labels and entry boxes for the items details 
+    item_name_label = Label(body_frame, text="item name: ")
+    item_name_label.grid(row=0, column=0, padx=10, pady=5, sticky="NSEW")
+    item_name_entry = Entry(body_frame)
+    item_name_entry.grid(row=0, column=1, padx=10, pady=5, sticky="NSEW")
+
+    item_price_label = Label(body_frame, text="estimated value: ")
+    item_price_label.grid(row=1, column=0, padx=10, pady=5, sticky="NSEW")
+    item_price_entry = Entry(body_frame)
+    item_price_entry.grid(row=1, column=1, padx=10, pady=5, sticky="NSEW")
+
+    item_condition_label = Label(body_frame, text="condition: ")
+    item_condition_label.grid(row=2, column=0, padx=10, pady=5, sticky="NSEW")
+    item_condition_entry = Entry(body_frame)
+    item_condition_entry.grid(row=2, column=1, padx=10, pady=5, sticky="NSEW")
+
+    # button and process to add items to your list
+    add_item_button = ttk.Button(body_frame, text="Add Item", command=add_item) 
+    add_item_button.grid(row=3, column=0, padx=10, pady=5, sticky="NSEW")
 
 def next_steps_page():
     root.withdraw()
@@ -49,7 +81,6 @@ def progression_page():
     # Title for the page placed into the header frame
     title = Label(header_frame, text="Like A Knife Through Clutter - progression", font=("papyrus", 20, "bold"))
     title.grid(row=0, column=0, padx=10, pady=10, sticky="NSEW")
-
 
 # frames for the page
 header_frame = ttk.LabelFrame(root) 
